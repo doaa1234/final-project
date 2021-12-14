85f03a1eb9ce0968840724103f4f2a4e8aff14ef
Aseel1818 <63041611+Aseel1818@users.noreply.github.com>
2021-12-14 22:14:24 +0200

    Update servera.js

--- servera.js
+++ servera.js
@@ -5,9 +5,9 @@ var bodyParser = require('body-parser');
 var db = require('./databasea');
 var Json2csvParser = require('json2csv').Parser;
 const fs = require('fs');
-
-
 var app = express();
+
+
 
 app.get('/export-csv', function (req, res) {
   db.query("SELECT * FROM users", function (err, users, fields) {
@@ -18,14 +18,32 @@ app.get('/export-csv', function (req, res) {
     console.log(jsonUsers);
 
     // -> Convert JSON to CSV data
-    const csvFields = ['email', 'UsersFname', 'UsersLname'];
+    class CONVERRT{
+      constructor(){
+        this.email='email';
+        this.UsersFname='UsersFname';
+        this.UsersLname='UsersLname';
+      }
+    }
+
+    const obj=new CONVERRT();
+
+    const csvFields = [obj.email, obj.UsersFname, obj.UsersLname];
     const json2csvParser = new Json2csvParser({ csvFields });
     const csv = json2csvParser.parse(jsonUsers);
 
     console.log(csv);
 
-    res.setHeader("Content-Type", "text/csv");
+
+    class RESULTS{
+      constructor(){
+        res.setHeader("Content-Type", "text/csv");
     res.setHeader("Content-Disposition", "attachment; filename=users.csv");
+      }
+    }
+
+    const objResult=new RESULTS();
+    objResult;
 
     res.status(200).end(csv);
     // -> Check 'users.csv' file in root project folder
@@ -33,8 +51,16 @@ app.get('/export-csv', function (req, res) {
 });
 
 // port must be set to 8080 because incoming http requests are routed from port 80 to port 8080
-app.listen(8000, function () {
+class PORT{
+  constructor(){
+    app.listen(8000, function () {
   console.log('Node app is running on port 8000');
 });
+  }
+}
+
+const port=new PORT();
+port;
+
 
 module.exports = app;
